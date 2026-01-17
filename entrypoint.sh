a22c736687558d48c0d451f6b876dba4c4d3474c
Matti Paksula <matti.paksula@iki.fi>
2026-01-17 12:00:03 +0200

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -eEuo pipefail
 
 pandoc index.md -o public/index.html \
   --toc --standalone -c styles.css \
-  --filter /opt/pandoc-crossref/pandoc-crossref
+  --filter /opt/pandoc-crossref/pandoc-crossref \
+   --extract-media=public
 
 echo "OK"
